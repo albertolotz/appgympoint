11feea2974a4349d1df9b2f14263a1c16b23bdb1
alberto lotz <alberto10lotz@gmail.com>
2019-11-13 18:21:40 -0300

    ajustee

---
+++ src/app/models/checkins.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Checkins extends Model {
+  static init(sequelize) {
+    super.init(
+      {},
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Students, { foreignKey: 'student_id' });
+  }
+}
+
+export default Checkins;
